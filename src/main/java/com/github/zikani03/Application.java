22221ff0890fac7d9f3acfc781d5e65d0452de13
Zikani Nyirenda Mwase <zikani.nmwase@ymail.com>
2017-07-18 13:28:12 +0200

    Update docblock

--- src/main/java/com/github/zikani03/Application.java
+++ src/main/java/com/github/zikani03/Application.java
@@ -4,7 +4,20 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 
 /**
- * Created by zikani on 4/1/17.
+ * Main class for the application
+ *
+ * multiclone [options] OWNER REPO [REPO] .. [REPO]
+ *
+ * Options
+ *
+ * -u --username   USERNAME a username to use in authentication with the git server, may prompt for password
+ *                 if the configured transport mechanism requires one (e.g. https)
+ * -f --from       github|bitbucket|ssh
+ * -d --directory  DIRECTORY
+ * -s --submodules Clone submodules as well
+ *    --shallow    Clone the repositories as shallow repos
+ * -v --verbose    Show verbose output
+ *
  */
 public class Application {
     public static void main(String... args) {
